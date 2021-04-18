c7bc7ef8dbb3e2580252694bca652c47e932fe25
Luke Spang <lspang20@gmail.com>
2021-04-18 19:04:37 -0400

    second sunday

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var button = document.getElementById('startbutton');
 var timerElement = document.getElementById('timer');
 var timerCount = 5;
 
+
+
 function startTimer(){
    var timer = setInterval(function(){
         timerCount--;
@@ -13,6 +15,34 @@ function startTimer(){
         }
     }, 1000)
 }
-
-
 button.addEventListener("click",startTimer);
+
+var questions = [
+    {
+        ques: "What is the name of the first Star Wars movie released in 1977?",
+        options: ['A New Hope', "Empire Strikes Back", "Revenge of the Sith", "Attack of the Clones"],
+        answer: "A New Hope"
+    },
+    {
+        ques: "What is the name of the first Star Wars movie released in 1977?",
+        options: ['A New Hope', "Empire Strikes Back", "Revenge of the Sith", "Attack of the Clones"],
+        answer: "A New Hope"
+    },
+    {
+        ques: "What is the name of the first Star Wars movie released in 1977?",
+        options: ['A New Hope', "Empire Strikes Back", "Revenge of the Sith", "Attack of the Clones"],
+        answer: "A New Hope"
+    },
+    {
+        ques: "What is the name of the first Star Wars movie released in 1977?",
+        options: ['A New Hope', "Empire Strikes Back", "Revenge of the Sith", "Attack of the Clones"],
+        answer: "A New Hope"
+    },
+];
+
+var score = 0;
+var questionPosition = 0;
+
+var questionDiv = document.getElementById("quiz");
+var quizQuestions = document.getElementById("ques");
+var quizAnswers = document.getElementById("options")
