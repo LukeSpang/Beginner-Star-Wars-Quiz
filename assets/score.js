9f8a8d02fcffa92a739a3c501f42fb6b5e32f9c3
Luke Spang <lspang20@gmail.com>
2021-04-23 14:17:09 -0400

    fixed js errors by adding if null statements to buttons

--- assets/score.js
+++ assets/score.js
@@ -3,14 +3,16 @@ var resetHStn = document.getElementById('clear');
 var tryAgainBtn = document.getElementById('tryAgain');
 
 //creates new list items and places high scores and initials from local storage into said list. 
-
+if (endScores === null){
+}else{
 for (let index = 0; index < endScores.length; index++) {
     var a = document.createElement('li');
     var b = document.createTextNode(`${index + 1}. ${endScores[index].playerInitials.toUpperCase()} - ${endScores[index].userScore}`);
     a.appendChild(b);
     document.querySelector('#playerList').appendChild(a);
     a.className += 'userList';
-}
+}}
+
 //compares current score with new incoming score and sets
 function betterThan(a,b){
     if (a.userScore < b.userScore){
@@ -22,12 +24,17 @@ function betterThan(a,b){
     return 0;
 }
 //takes you back to homepage to take quiz again
+if (tryAgainBtn === null){
+}else{
 tryAgainBtn.addEventListener('click', function(event){
     console.log(event)
     window.location.replace("./index.html")
-});
+})};
 // resets the local storage, will display no old high scores
+if(resetHStn === null){
+
+}else{
 resetHStn.addEventListener('click', function(){
     localStorage.clear();
     document.getElementById('playerList').innerHTML = ""
-})
+})};
